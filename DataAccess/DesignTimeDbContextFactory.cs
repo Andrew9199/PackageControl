073b6fd0dbda23a6ebf25a09021b27ce2036ed1f
Jack9199 <jack91992002@gmail.com>
2024-09-06 13:43:00 -0500

    Identity funcionando y base de datos basica sininformacion lista

---
+++ DataAccess/DesignTimeDbContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PackageControlDataContext>
+{
+    public PackageControlDataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PackageControlDataContext>();
+        optionsBuilder.UseSqlServer("Server=DESKTOP-7BMEFPA\\SQLEXPRESS;Database=DocumentGenerator2;User Id=sa;Password=sa;Encrypt=False;");
+
+        return new PackageControlDataContext(optionsBuilder.Options);
+    }
+}
---
+++ DataAccess/PackageControlDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class PackageControlDataContext : IdentityDbContext
+    {
+        public PackageControlDataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
