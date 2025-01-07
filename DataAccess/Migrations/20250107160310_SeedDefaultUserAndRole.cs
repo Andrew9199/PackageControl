032b8e4e83e1ed9ed069426243ee0e9942cb44d5
“Alexis <alexissanchezd97@gmail.com>
2025-01-07 11:06:03 -0500

    Agregado el usuario por default

---
+++ DataAccess/Migrations/20250107160310_SeedDefaultUserAndRole.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDefaultUserAndRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "073401da-110e-4962-afdd-2a966d1f0d7b", null, "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "IdentificationNumber", "IdentificationType", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "9f19a2b2-73a1-4fb1-9529-dca5b147151a", 0, "72956ea5-a815-4093-b409-31b3f225680f", "alex@gmail.com", true, "Alex", "1999999999", "Cedula", "Will", false, null, "ALEX@GMAIL.COM", "ALEX", "AQAAAAIAAYagAAAAEJCYB5eaPrSlWSpmizKppE/ThMBll4zUfiqKorrlTqIJESDY0dD5e75HSOUimnzpmQ==", null, false, "3MDBYHN34YWQIJOD6LHXCBAZ33SQOASS", false, "Alex" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "073401da-110e-4962-afdd-2a966d1f0d7b", "9f19a2b2-73a1-4fb1-9529-dca5b147151a" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "073401da-110e-4962-afdd-2a966d1f0d7b", "9f19a2b2-73a1-4fb1-9529-dca5b147151a" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "073401da-110e-4962-afdd-2a966d1f0d7b");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "9f19a2b2-73a1-4fb1-9529-dca5b147151a");
+        }
+    }
+}
--- DataAccess/PackageControlDataContext.cs
+++ DataAccess/PackageControlDataContext.cs
@@ -51,6 +51,45 @@ namespace DataAccess
                 // La relación con ApplicationUser ya está definida en ApplicationUser
                 // No es necesario volver a configurarla aquí.
             });
+            // Datos semilla para el rol
+            var adminRoleId = "073401da-110e-4962-afdd-2a966d1f0d7b";
+            var adminUserId = "9f19a2b2-73a1-4fb1-9529-dca5b147151a";
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Id = adminRoleId,
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            });
+
+            // Datos semilla para el usuario
+            modelBuilder.Entity<ApplicationUser>().HasData(new ApplicationUser
+            {
+                Id = "9f19a2b2-73a1-4fb1-9529-dca5b147151a",
+                UserName = "Alex",
+                NormalizedUserName = "ALEX",
+                Email = "alex@gmail.com",
+                NormalizedEmail = "ALEX@GMAIL.COM",
+                EmailConfirmed = true,
+                PasswordHash = "AQAAAAIAAYagAAAAEJCYB5eaPrSlWSpmizKppE/ThMBll4zUfiqKorrlTqIJESDY0dD5e75HSOUimnzpmQ==",
+                //Contraseña: @Aa123
+                SecurityStamp = "3MDBYHN34YWQIJOD6LHXCBAZ33SQOASS",
+                ConcurrencyStamp = "72956ea5-a815-4093-b409-31b3f225680f",
+                PhoneNumber = null,
+                PhoneNumberConfirmed = false,
+                FirstName = "Alex",
+                LastName = "Will",
+                IdentificationNumber = "1999999999",
+                IdentificationType = "Cedula"
+            });
+
+
+            // Relación usuario-rol
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
+            {
+                UserId = adminUserId,
+                RoleId = adminRoleId
+            });
         }
     }
 }
